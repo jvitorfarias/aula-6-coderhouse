ddc0c5e23f6f8090f9f246101a44862f1f100cf2
jvitorfarias <joaovitorfarias4@gmail.com>
2023-06-23 16:34:34 -0300

    first commit

---
+++ aula6.js
@@ -0,0 +1,28 @@
+const vw = [
+  "Gol",
+  "Polo",
+  "Golf",
+  "Jetta"
+]
+
+const gm = [
+  "Celta",
+  "Corsa",
+  "Vectra"
+]
+
+const ford = [
+  "Ka",
+  "Fiesta",
+  "Focus"
+]
+
+const honda = [
+  "Civic",
+  "Fit",
+  "Prelude",
+  "Accord"
+]
+
+const primeiroProjetoTurbo = vw.concat(gm, ford, honda)
+console.log(primeiroProjetoTurbo)
